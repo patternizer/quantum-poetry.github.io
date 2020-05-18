25c7b602bd87ed6dbb5a39315fffa6ef3313c2fc
patternizer <patternizer@gmail.com>
2020-05-18 15:54:19 +0100

    Nuanced handling of sentences and extracted lines. Word frequency used to identify knots.

--- quantum_poetry.py
+++ quantum_poetry.py
@@ -22,17 +22,6 @@ from collections import Counter
 #-----------------------------------------------------------------------
 # METHODS
 #-----------------------------------------------------------------------
-def extract_unique(input_file, output_file):
-    """
-    Write unique word list to file
-    """
-    z = []
-    with open(input_file,'r') as fileIn, open(output_file,'w') as fileOut:
-        for line in fileIn:
-            for word in line.split():
-                if word not in z:
-                    z.append(word)
-                    fileOut.write(word+'\n')
                  
 def word_in_line(word, line):
     """
@@ -59,58 +48,68 @@ def word_in_line(word, line):
 #-----------------------------------------------------------------------
 
 input_file = 'poem.txt'
-output_file = 'unique_words.txt'
-
-# Parse text and output unique words to .txt                
-extract_unique(input_file, output_file)
 
 # Store text as a single string and lines in a list
 textstr = ''
-lines = []
+linelist = []
 with open (input_file, 'rt') as f:      
-    for line in f:                      
-        lines.append(line.rstrip('\n'))
-        textstr = textstr + line.rstrip('\n') + ' '
+    for line in f:   
+        if len(line)>1: # ignore empty lines                 
+            linelist.append(line.rstrip('\n'))
+            textstr = textstr + line.rstrip('\n')
+        else:
+            continue
+
+# extract sentences into list
+sentencelist = textstr.split('.')
+sentencelist = sentencelist[0:-1] # ignore last entry which is '' due to final full stop
 
 # Clean text and lower case all words
-for char in '-.,\n':
-    textstr = textstr.replace(char,' ')
-textstr = textstr.lower() 
-wordlist = textstr.split()
-
-# Store unique words in an array
-g = open(output_file, 'r')
-word_list = g.readlines()
-uniquewords = [x.strip() for x in word_list] 
-
-# Word and line counts
-nwords = len(wordlist)         # --> 222
-nlines = len(lines)            # --> 26
-nunique = len(unique_words)    # --> 140
-
-# Word frequencies
-word_freq = Counter(wordlist).most_common() # --> word_freq[0][0] = 'the' and word_freq[0][1] = '13'
-
-print(textstr)
-print(wordlist)
-print(uniquewords)
-print('N(words)=', nwords)
-print('N(lines)=', nlines)
-print('N(unique)=', nunique)
-
-# Word index test
-index = 0
-prev = 0                      
-str = lines[0]          
-
+str = textstr
 for char in '-.,\n':
     str = str.replace(char,' ')
 str = str.lower() 
-linewordlist = str.split()
+wordlist = str.split()
 
-substr = unique_words[0].lower()
-index = str.find(substr)  # set index to first occurrence of substr
-print(index)
+# Store unique words in an array
+uniquewordlist = []
+for word in wordlist:           
+    if word not in uniquewordlist:
+        uniquewordlist.append(word)
+                    
+# Word frequencies
+wordfreq = Counter(wordlist).most_common() # --> wordfreq[0][0] = 'the' and wordfreq[0][1] = '13'
+
+# Find knots having word frequency > 1
+knotlist = []
+for i in range(len(wordfreq)):
+    if wordfreq[i][1] > 1:
+        knotlist.append(wordfreq[i][0])
+    else:
+        continue
+
+# Counts
+nsentences = len(sentencelist)    # --> 
+nlines = len(linelist)            # --> 26
+nwords = len(wordlist)            # --> 222
+nunique = len(uniquewordlist)     # --> 134
+nknots = len(knotlist)           # --> 30
+
+print(textstr)
+print(sentencelist)
+print(linelist)
+print(wordlist)
+print(uniquewordlist)
+print(knotlist)
+
+print('N(sentences)=', nsentences)
+print('N(lines)=', nlines)
+print('N(words)=', nwords)
+print('N(unique)=', nunique)
+print('N(knots)=', nknots)
+
+# substr = uniquewords[0].lower()
+# index = str.find(substr)  # set index to first occurrence of substr
 
 
 #-----------------------------------------------------------------------
